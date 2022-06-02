7d6352906cb7415324269d89f45c437c57c43517
César Córdoba <cordobacesar29@gmail.com>
2022-06-02 16:13:43 -0300

    create empleado component from CLI

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+  nombre="Juan";
+  apellido="Diaz";
+  edad=20;
+  empresa="Google";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
